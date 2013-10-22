be0d0c807adb5bf9316ce9726fdee618d1168580
PaulMag <paulms@ulrik.uio.no>
2013-10-22 15:11:55 +0200

    Did: Fill F matrix(cube) with forces and adds the transpose.

--- SolSys.cpp
+++ SolSys.cpp
@@ -10,26 +10,28 @@ using namespace arma;
 #include "CelObj.h"
 
 SolSys:: SolSys() {
-    bodies [N];
-    CelObj a = bodies[2];
-    a.mass = 17;
-    cout << a.mass << endl;
+    bodies[N];
+    //CelObj a = bodies[2];
+    //a.mass = 17;
+    //cout << a.mass << endl;
     //CelObj b = bodies[2];
     //cout << b.mass << endl;
 }
 
 SolSys:: ~SolSys() {
-    //delete[];
+    delete [] bodies;
 }
 
 void SolSys:: findForces() {
     /* Fills a matrix with the forces between every CelObj.
      */
-    cube forces = zeros<cube>(N,N,3);
-    vec a = randu<vec>(3);
+    cube F = zeros<cube>(N,N,3);
     for (int i=0; i<N; i++) {
         for (int j=0; j<i; j++) {
-            forces(span(i), span(j), span()) = a;
+            F(span(i), span(j), span()) = bodies[i].getForce(bodies[j]);
         }
     }
+    F.slice(0) += F.slice(0).t();
+    F.slice(1) += F.slice(1).t();
+    F.slice(2) += F.slice(2).t();
 }
