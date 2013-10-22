eaccd8c1ebfe981cbc136b7b946414f60eb9a5ae
PaulMag <paulms@ulrik.uio.no>
2013-10-22 20:33:28 +0200

    Added: Methods for setting pos and vel, methods for getting pos and vel (unfinished), method for finding accelerations(contains an error) from forces. Added: RK4 method for forwarding entire system one timestep.

--- SolSys.cpp
+++ SolSys.cpp
@@ -22,7 +22,35 @@ SolSys:: ~SolSys() {
     delete [] bodies;
 }
 
-void SolSys:: findForces() {
+void SolSys:: setPositions(mat x) {
+    for (int i=0; i<N; i++) {
+        bodies[i].position = x.row(i);
+    }
+}
+
+void SolSys:: setVelocities(mat v) {
+    for (int i=0; i<N; i++) {
+        bodies[i].position = v.row(i);
+    }
+}
+
+mat SolSys:: getPositions() {
+    mat x(N,3);
+    for (int i=0; i<N; i++) {
+        // TODO
+    }
+    return x;
+}
+
+mat SolSys:: getVelocities() {
+    mat v(N,3);
+    for (int i=0; i<N; i++) {
+        // TODO
+    }
+    return v;
+}
+
+cube SolSys:: findForces() {
     /* Fills a matrix with the forces between every CelObj.
      */
     cube F = zeros<cube>(N,N,3);
@@ -31,7 +59,51 @@ void SolSys:: findForces() {
             F(span(i), span(j), span()) = bodies[i].getForce(bodies[j]);
         }
     }
-    F.slice(0) += F.slice(0).t();
-    F.slice(1) += F.slice(1).t();
-    F.slice(2) += F.slice(2).t();
+    F.slice(0) -= F.slice(0).t(); // mirror forces in the matrix
+    F.slice(1) -= F.slice(1).t();
+    F.slice(2) -= F.slice(2).t();
+    F *= G; // multiply with gravity constant. TODO: Add gravity constant G
+    return F;
+}
+
+mat SolSys:: findAccels() {
+    cube F = findForces();
+    mat a(N,3);
+
+    for (int i=0; i<N; i++) {
+        a.row(i) = sum(F(span(i), span(), span()), dim=1) / bodies[i].mass;
+            /* TODO: Not sure if this is the correct sum. Control check later!
+             *       dim doesn't work in this context.
+             */
+    }
+    return a;
+}
+
+void SolSys:: rungeKutta4() {
+    /* Forwards every Celestial Object in the Solar System one timestep with
+     * the RungeKutta4 method.
+     * TODO: Implement timestep h somehow.
+     */
+
+    mat v0 = getVelocities();
+    mat x0 = getPositions();
+    mat a0 = findAccels();
+
+    mat v1 = v0 + a0 * h2;
+    mat x1 = x0 + v0 * h2;
+    setPositions(x1);
+    mat a1 = findAccels();
+
+    mat v2 = v1 + a1 * h2;
+    mat x2 = v1 + v1 * h2;
+    setPositions(x2);
+    mat a2 = findAccels();
+
+    mat v3 = v2 + a2 * h;
+    mat x3 = x2 + x2 * h;
+    setPositions(x3);
+    mat a3 = findAccels();
+
+    setVelocities(v0 + (1./6) * (a0 + 2*a1 + 2*a2 + a3) * h);
+    setPositions (x0 + (1./6) * (v0 + 2*v1 + 2*v2 + v3) * h);
 }
