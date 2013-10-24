f6f9f0bbb290f4d76a4a2ed6936a02ac514a4123
PaulMag <paulms@ulrik.uio.no>
2013-10-24 11:25:46 +0200

    Kristoffers furious attempt to make his make.py work again.

--- make.py
+++ make.py
@@ -1,6 +1,6 @@
 # "How does one use this crap?" - Vedad Hodzic
 
 import os,sys
-os.system('c++ -c *.cpp')
+os.system('c++ -c *.cpp -larmadillo -lblas -llapack')
 os.system('c++ -o %s.exe -larmadillo -lblas -llapack *.o' % sys.argv[1])
 os.system('rm *.o')
